5ea0554f6098b253326e7753a01eacb0445a45e3
sharryXheisenberg <pandesaurabh4596@gmail.com>
2025-10-08 14:21:00 +0530

    Day 1 learning

---
+++ oop_fundamentals/typescript_implementation/classes.js
@@ -0,0 +1,30 @@
+"use strict";
+// // A class is a blueprint or template. It defines the properties (attributes) and behaviors (methods) that the objects created from it will have.
+Object.defineProperty(exports, "__esModule", { value: true });
+// class car{
+//     private brand: string;
+//     private model: string;
+//     private speed: number;
+//     constructor(brand:string , model:string){
+//         this.brand = brand;
+//         this.model = model;
+//         this.speed = 0;
+//     }
+//     accelerate(increment:number):void{
+//         this.speed+=increment;
+//     }
+//     displayStatus():void{
+//         console.log(`${this.brand} is running at ${this.speed} and model is: ${this.model}`);
+//     }
+// }
+// function main():void{
+// const BMW = new car("BWM" , "M5");
+// const Corvette = new car("Corvette","HellCat");
+// BMW.accelerate(30);
+// Corvette.accelerate(40);
+// BMW.displayStatus();
+// Corvette.displayStatus();
+// main();
+// }
+console.log(20);
+//# sourceMappingURL=classes.js.map
